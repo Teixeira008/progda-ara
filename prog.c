c8fea20be220f1a6f43cc386195a4d8e61a5bfb3
Teixeira008 <166056748+Teixeira008@users.noreply.github.com>
2024-11-29 16:51:16 -0300

    Add files via upload

---
+++ prog.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+int main()
+{
+ 
+ int a;
+ int b;
+ int c;
+ int d;
+ int e; 
+ 
+ 
+ 
+ printf("Digite um número numero aleatório\n");
+ scanf("%d", &a); 
+ 
+  printf("Digite um número numero aleatório\n");
+ scanf("%d", &b);
+  
+  printf("Digite um número numero aleatório\n");
+ scanf("%d", &c);
+ 
+  printf("Digite um número numero aleatório\n");
+ scanf("%d", &d);
+ 
+  printf("Digite um número numero aleatório\n");
+ scanf("%d", &e);
+ 
+ 
+    if(a<0){
+     printf("Seu numero é negativo\n");
+    } else{
+        printf("Seu numero é Positivo\n");
+    }
+    if(b<0){
+     printf("Seu numero é negativo\n");
+    } else{
+        printf("Seu numero é Positivo\n");
+    }
+    if(c<0){
+     printf("Seu numero é negativo\n");
+    } else{
+        printf("Seu numero é Positivo\n");
+        }
+    if(d<0){
+     printf("Seu numero é negativo\n");
+    } else{
+        printf("Seu numero é Positivo\n");
+        }
+    if(e<0){
+     printf("Seu numero é negativo\n");
+    } else{
+        printf("Seu numero é Positivo\n");
+        }
+    
+
+ 
+
+}
